5374c752235cd3874da8832d61fba62b41089921
RamSSi <001229aram@gmail.com>
2022-11-08 10:53:45 +0900

    chore: 기본 패키지 구성

---
+++ soup/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from './components/views/LandingPage/LandingPage'; 
+import LoginPage from './components/views/LoginPage/LoginPage'
+import JoinPage from './components/views/JoinPage/JoinPage';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/join" element={<JoinPage />} />
+          {/* <Route exact path="*" element={<NotFound />} */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
