95de525b7087e1008ade98a20ae24077d237cd0d
RamSSi <001229aram@gmail.com>
2022-11-24 15:59:24 +0900

    feat: MSA proxy 기능

--- soup/src/setupProxy.js
+++ soup/src/setupProxy.js
@@ -1,45 +1,17 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 // const SOUP_API_ADDR = process.env.API_IP;
-const SOUP_API_ADDR = "127.0.0.1";
-const BACK_URI = `http://${SOUP_API_ADDR}:8000`;
+// const SOUP_API_ADDR = "127.0.0.1";
+const member = `http://${process.env.MEMBER_URI}`;
+const admin = `http://${process.env.ADMIN_URI}`;
+const search = `http://${process.env.SEARCH_URI}`;
 
 module.exports = function(app) {
-    app.use(
-        "/ ",
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
-
-    app.use(
-        "/main",
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
 
     app.use(
         "/members",
         createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
-
-    app.use(
-        "/search",
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
-    app.use(
-        "/admin",
-        createProxyMiddleware({
-            target: BACK_URI,
+            target: member,
             changeOrigin: true,
         })
     );
@@ -47,7 +19,22 @@ module.exports = function(app) {
     app.use(
         "/bot",
         createProxyMiddleware({
-            target: BACK_URI,
+            target: search,
+            changeOrigin: true,
+        })
+    );
+
+    app.use(
+        "/search",
+        createProxyMiddleware({
+            target: search,
+            changeOrigin: true,
+        })
+    );
+    app.use(
+        "/admin",
+        createProxyMiddleware({
+            target: admin,
             changeOrigin: true,
         })
     );
