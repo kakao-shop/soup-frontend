fa14d647971cfbb21530121830d64a01fccbeb8e
RamSSi <001229aram@gmail.com>
2022-11-25 11:24:55 +0900

    feat: msa proxy

--- soup/src/setupProxy.js
+++ soup/src/setupProxy.js
@@ -1,7 +1,45 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 // const SOUP_API_ADDR = process.env.API_IP;
-// const SOUP_API_ADDR = "127.0.0.1";
+// // const SOUP_API_ADDR = "localhost";
+// const BACK_URI = `http://${SOUP_API_ADDR}:8000`;
+
+// module.exports = function(app) {
+
+//     app.use(
+//         "/members",
+//         createProxyMiddleware({
+//             target: BACK_URI,
+//             changeOrigin: true,
+//         })
+//     );
+
+//     app.use(
+//         "/admin",
+//         createProxyMiddleware({
+//             target: BACK_URI,
+//             changeOrigin: true,
+//         })
+//     );
+
+//     app.use(
+//         "/search",
+//         createProxyMiddleware({
+//             target: BACK_URI,
+//             changeOrigin: true,
+//         })
+//     );
+
+
+//     app.use(
+//         "/bot",
+//         createProxyMiddleware({
+//             target: BACK_URI,
+//             changeOrigin: true,
+//         })
+//     );
+// };
+
 const member = `http://${process.env.MEMBER_URI}`;
 const admin = `http://${process.env.ADMIN_URI}`;
 const search = `http://${process.env.SEARCH_URI}`;
