bb5236c042d7acdc13956a480f223e75e7ab045d
RamSSi <001229aram@gmail.com>
2022-11-26 15:50:32 +0900

    feat: 백엔드 MSA proxy

--- soup/src/setupProxy.js
+++ soup/src/setupProxy.js
@@ -1,62 +1,14 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// const SOUP_API_ADDR = process.env.API_IP;
-const SOUP_API_ADDR = "localhost";
-const BACK_URI = `http://${SOUP_API_ADDR}:8000`;
-
-module.exports = function(app) {
-
-    app.use(
-        "/members",
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
-
-    app.use(
-        "/admin",
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
-
-    app.use(
-        "/search",
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
-
-
-    app.use(
-        "/bot",
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
-};
-
-// const member = `http://${process.env.MEMBER_URI}`;
-// const admin = `http://${process.env.ADMIN_URI}`;
-// const search = `http://${process.env.SEARCH_URI}`;
+// const SOUP_API_ADDR = "localhost";
+// const BACK_URI = `http://${SOUP_API_ADDR}:8000`;
 
 // module.exports = function(app) {
 
 //     app.use(
 //         "/members",
 //         createProxyMiddleware({
-//             target: member,
-//             changeOrigin: true,
-//         })
-//     );
-
-//     app.use(
-//         createProxyMiddleware(["/search", "/bot"], {
-//             target: search,
+//             target: BACK_URI,
 //             changeOrigin: true,
 //         })
 //     );
@@ -64,8 +16,55 @@ module.exports = function(app) {
 //     app.use(
 //         "/admin",
 //         createProxyMiddleware({
-//             target: admin,
+//             target: BACK_URI,
+//             changeOrigin: true,
+//         })
+//     );
+
+//     app.use(
+//         "/search",
+//         createProxyMiddleware({
+//             target: BACK_URI,
+//             changeOrigin: true,
+//         })
+//     );
+
+
+//     app.use(
+//         "/bot",
+//         createProxyMiddleware({
+//             target: BACK_URI,
 //             changeOrigin: true,
 //         })
 //     );
 // };
+
+const member = `http://${process.env.MEMBER_URI}`;
+const admin = `http://${process.env.ADMIN_URI}`;
+const search = `http://${process.env.SEARCH_URI}`;
+
+module.exports = function(app) {
+
+    app.use(
+        "/members",
+        createProxyMiddleware({
+            target: member,
+            changeOrigin: true,
+        })
+    );
+
+    app.use(
+        createProxyMiddleware(["/search", "/bot"], {
+            target: search,
+            changeOrigin: true,
+        })
+    );
+
+    app.use(
+        "/admin",
+        createProxyMiddleware({
+            target: admin,
+            changeOrigin: true,
+        })
+    );
+};
