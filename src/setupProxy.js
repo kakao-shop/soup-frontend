d2167346cb05c9edfa7b12c594152b68f8deab83
krr3804 <72504977+krr3804@users.noreply.github.com>
2022-12-01 13:52:06 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,70 +1,34 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const BACK_URI = `http://${process.env.SOUP_API}:8000`;
+//const BACK_URI = `http://${process.env.SOUP_API}:8000`;
 // const BACK_URI = `http://localhost:8000`;
+
+const member = `http://${process.env.MEMBER_URI}`;
+const admin = `http://${process.env.ADMIN_URI}`;
+const search = `http://${process.env.SEARCH_URI}`;
 
 module.exports = function(app) {
 
-    app.use(
-        "/members",
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
+     app.use(
+         "/members",
+         createProxyMiddleware({
+             target: member,
+             changeOrigin: true,
+         })
+     );
 
-    app.use(
-        "/admin",
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
+     app.use(
+         createProxyMiddleware(["/search", "/bot"], {
+             target: search,
+             changeOrigin: true,
+         })
+     );
 
-    app.use(
-        "/search",
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
-
-
-    app.use(
-        "/bot",
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
-};
-
-// const member = `http://${process.env.MEMBER_URI}`;
-// const admin = `http://${process.env.ADMIN_URI}`;
-// const search = `http://${process.env.SEARCH_URI}`;
-
-// module.exports = function(app) {
-
-//     app.use(
-//         "/members",
-//         createProxyMiddleware({
-//             target: member,
-//             changeOrigin: true,
-//         })
-//     );
-
-//     app.use(
-//         createProxyMiddleware(["/search", "/bot"], {
-//             target: search,
-//             changeOrigin: true,
-//         })
-//     );
-
-//     app.use(
-//         "/admin",
-//         createProxyMiddleware({
-//             target: admin,
-//             changeOrigin: true,
-//         })
-//     );
-// };
+     app.use(
+         "/admin",
+         createProxyMiddleware({
+             target: admin,
+             changeOrigin: true,
+         })
+     );
+ };
