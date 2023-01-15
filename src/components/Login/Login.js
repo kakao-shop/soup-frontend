12a7730b7c07b667def7b79cd82857c9060ce642
강아람 <001229aram@gmail.com>
2023-01-15 17:05:25 +0900

    feat: LoginPage 추가
    2023-01-15 Login 컴포넌트 생성, LoginPage 라우팅

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,3 +1,29 @@
-const Login = (props) => {};
+import { Link } from "react-router-dom";
+
+import Panel from "../UI/Panel/Panel";
+
+import classes from "./Login.module.css";
+
+const Login = (props) => {
+  return (
+    <Panel>
+      <h2>로그인</h2>
+      <form>
+        <div className="form-input">
+          <label htmlFor="login-id">아이디</label>
+          <input type="text" minLength="5" maxLength="12" id="login-id" />
+        </div>
+        <div className="form-input">
+          <label htmlFor="login-pw">비밀번호</label>
+          <input type="password" minLength="6" maxLength="15" id="login-pw" />
+        </div>
+        <button>로그인</button>
+      </form>
+      <Link to="/join" className={classes["join-link"]}>
+        아직 회원이 아니신가요?
+      </Link>
+    </Panel>
+  );
+};
 
 export default Login;
