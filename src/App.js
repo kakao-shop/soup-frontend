1efe853d3e9a542b6e1ef9a06916738634a358b3
강아람 <001229aram@gmail.com>
2023-01-15 17:03:16 +0900

    feat: router 추가
    2023-01-15 메인, 로그인, 회원가입 경로 설정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/Layout/Header/Header";
 import Nav from "./components/Layout/Nav/Nav";
+
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import JoinPage from "./pages/JoinPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/join" element={<JoinPage />} />
+      </Routes>
     </div>
   );
 }
