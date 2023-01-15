36e9cff6d6c2ce31bed9b47b9284f0eea7aa3f8c
강아람 <001229aram@gmail.com>
2023-01-15 17:04:59 +0900

    feat: JoinPage 추가
    2023-01-15 Join 컴포넌트 생성, JoinPage 라우팅

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,3 +1,80 @@
-const Join = (props) => {};
+import Panel from "../UI/Panel/Panel";
+
+import classes from "./Join.module.css";
+
+const Join = (props) => {
+  return (
+    <Panel>
+      <h2>회원가입</h2>
+      <form>
+        <div className="form-input">
+          <label>아이디</label>
+          <div className={classes["join-id"]}>
+            <input
+              id="join-id"
+              type="text"
+              minLength="5"
+              maxLength="12"
+              placeholder="ID 입력 (5~12자)"
+            />
+            <button className={classes["dup-btn"]}>중복확인</button>
+          </div>
+        </div>
+        <div className="form-input">
+          <label>닉네임</label>
+          <input
+            type="text"
+            minLength="2"
+            maxLength="10"
+            id="join-nickname"
+            placeholder="닉네임 입력 (2~10자)"
+          />
+        </div>
+        <div className="form-input">
+          <label>비밀번호</label>
+          <input
+            type="password"
+            minLength="6"
+            maxLength="15"
+            id="join-pw"
+            placeholder="비밀번호 입력 (6~15자)"
+          />
+        </div>
+        <div className="form-input">
+          <label>비밀번호 확인</label>
+          <input
+            type="password"
+            minLength="6"
+            maxLength="15"
+            id="join-pwcheck"
+            placeholder="비밀번호 재입력"
+          />
+        </div>
+        <div className="form-input">
+          <label>생년월일</label>
+          <input
+            type="text"
+            id="join-birth"
+            minLength="8"
+            maxLength="8"
+            placeholder="ex) 19910101"
+          />
+        </div>
+        <div className="form-input">
+          <label>성별</label>
+          <div className={classes["join-gender"]}>
+            <input type="radio" value="M" id="male" />
+            <label>남</label>
+          </div>
+          <div className={classes["join-gender"]}>
+            <input type="radio" value="W" id="female" />
+            <label>여</label>
+          </div>
+        </div>
+        <button>회원가입</button>
+      </form>
+    </Panel>
+  );
+};
 
 export default Join;
--- src/components/UI/Container/Container.js
+++ src/components/UI/Container/Container.js
@@ -1,7 +1,7 @@
 import classes from "./Container.module.css";
 
 const Container = (props) => {
-  return <div className={classes.container}>123{props.children}</div>;
+  return <div className={classes.container}>{props.children}</div>;
 };
 
 export default Container;
