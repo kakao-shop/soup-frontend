9c3b5d128c4d2acf456b3645b355b364fbddc0d1
강아람 <001229aram@gmail.com>
2023-01-17 13:17:20 +0900

    feat: Recommends 컴포넌트 생성
    2023-01-17 메인 화면에 사용할 아이템 추천 기능 Wrapper 컴포넌트

---
+++ src/components/Main/Recommends/Recommends.js
@@ -0,0 +1,13 @@
+import MainItems from "./MainItems";
+import MainItemTitle from "./MainItemTitle";
+
+const Recommends = (props) => {
+  return (
+    <div>
+      <MainItemTitle />
+      <MainItems />
+    </div>
+  );
+};
+
+export default Recommends;
