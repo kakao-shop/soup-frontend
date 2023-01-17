063e90a39d09e74fc467a64430ea0bdf54ef13ee
강아람 <001229aram@gmail.com>
2023-01-17 13:01:36 +0900

    feat: Nav 컴포넌트 추가
    2023-01-15 Nav 컴포넌트 디자인1, 2 모두 완료

--- src/components/Layout/Nav/Nav.js
+++ src/components/Layout/Nav/Nav.js
@@ -1,20 +1,24 @@
 import Container from "../../UI/Container/Container";
 
+import classes from "./Nav.module.css";
+
 const Nav = (props) => {
   return (
-    <Nav>
-      <ul>
-        <li>과일</li>
-        <li>채소</li>
-        <li>축산</li>
-        <li>수산·건어물</li>
-        <li>즉석식품/양념</li>
-        <li>냉장·냉동식품</li>
-        <li>생수/음료</li>
-        <li>빵/과자</li>
-        <li>쌀·잡곡</li>
-      </ul>
-    </Nav>
+    <nav>
+      <Container>
+        <ul className={classes["nav-ul"]}>
+          <li className="nav-item">과일</li>
+          <li className="nav-item">채소</li>
+          <li className="nav-item">축산</li>
+          <li className="nav-item">수산·건어물</li>
+          <li className="nav-item">즉석식품/양념</li>
+          <li className="nav-item">냉장·냉동식품</li>
+          <li className="nav-item">생수/음료</li>
+          <li className="nav-item">빵/과자</li>
+          <li className="nav-item">쌀·잡곡</li>
+        </ul>
+      </Container>
+    </nav>
   );
 };
 
