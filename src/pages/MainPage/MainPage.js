af1af8adbc164ff57febc12814c879d950c899c3
강아람 <001229aram@gmail.com>
2023-01-17 13:19:54 +0900

    feat: MainPage 컴포넌트 생성
    2023-01-17 Themes, Recommends 컴포넌트 추가

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import Themes from "../../components/Main/Themes/Themes";
+import Recommends from "../../components/Main/Recommends/Recommends";
+
+const MainPage = (props) => {
+  return (
+    <main>
+      <Themes />
+      <Recommends />
+    </main>
+  );
+};
+
+export default MainPage;
