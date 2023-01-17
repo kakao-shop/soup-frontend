4043cad041cd57ffc3868400d0da4ad88454fa28
강아람 <001229aram@gmail.com>
2023-01-17 13:02:18 +0900

    feat: Banner 컴포넌트 추가
    2023-01-17 Banner 이미지를 담을 컴포넌트, 이미지 추가

---
+++ src/components/Main/Themes/Banner.js
@@ -0,0 +1,19 @@
+const Banner = (props) => {
+  const BannerList = [
+    { id: 1, path: "/Banner/다이어트.png", title: "다이어트 테마" },
+    // { id: 2, path: "/Banner/밀키트.png" },
+    // { id: 3, path: "/Banner/빵.png" },
+    // { id: 4, path: "/Banner/음료.png" },
+    // { id: 5, path: "/Banner/카페.png" },
+  ].map((banner) => (
+    <img
+      src={`${process.env.PUBLIC_URL}/assets` + banner.path}
+      alt={banner.title}
+      key={banner.id}
+    />
+  ));
+
+  return <div>{BannerList}</div>;
+};
+
+export default Banner;
