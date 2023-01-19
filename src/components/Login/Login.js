2f5db33a5d25187994c1826bb0f9054f177f46d4
강아람 <001229aram@gmail.com>
2023-01-19 23:33:09 +0900

    feat: login 로직 추가
    2023-01-19 id, 비밀번호 제출 로직 구현중!

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,17 +5,43 @@ import Panel from "../UI/Panel/Panel";
 import classes from "./Login.module.css";
 
 const Login = (props) => {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const handleIdChange = (e) => {
+    setId(e.target.value);
+  };
+  const handlePwChange = (e) => {
+    setPw(e.target.value);
+  };
+
+  const handleLoginFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <Panel>
       <h2>로그인</h2>
-      <form>
+      <form onSubmit={handleLoginFormSubmit}>
         <div className="form-input">
           <label htmlFor="login-id">아이디</label>
-          <input type="text" minLength="5" maxLength="12" id="login-id" />
+          <input
+            type="text"
+            value={id}
+            minLength="5"
+            maxLength="12"
+            id="login-id"
+          />
         </div>
         <div className="form-input">
           <label htmlFor="login-pw">비밀번호</label>
-          <input type="password" minLength="6" maxLength="15" id="login-pw" />
+          <input
+            type="password"
+            value={pw}
+            minLength="6"
+            maxLength="15"
+            id="login-pw"
+          />
         </div>
         <button>로그인</button>
       </form>
