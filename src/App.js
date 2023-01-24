40c979d6e8a7b8144d7415ae46ee624aecbadad4
강아람 <001229aram@gmail.com>
2023-01-24 23:10:45 +0900

    feat: 페이지 라우팅 추가
    2023-01-24 장바구니, 마이페이지, 관리자 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { useContext, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AuthContext from "./store/auth-context";
 
 import Header from "./components/Layout/Header/Header";
 import Nav from "./components/Layout/Nav/Nav";
@@ -7,8 +9,13 @@ import Nav from "./components/Layout/Nav/Nav";
 import MainPage from "./pages/MainPage/MainPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import JoinPage from "./pages/JoinPage/JoinPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import CartPage from "./pages/CartPage/CartPage";
+import AdminPage from "./pages/AdminPage/AdminPage";
 
 function App() {
+  // const authCtx = useContext(AuthContext);
+
   return (
     <div className="App">
       <Header />
@@ -17,6 +24,9 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/join" element={<JoinPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/admins" element={<AdminPage />} />
       </Routes>
     </div>
   );
