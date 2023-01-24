a38a55132a565a0160a5485cd68fe631cd483be9
강아람 <001229aram@gmail.com>
2023-01-24 23:16:04 +0900

    fix: auth Context 를 이용하여 아이템 제목 변경
    2023-01-24 context 내의 로그인 여부 에 따라 출력되는 제목 변경

--- src/components/Main/Recommends/MainItemTitle.js
+++ src/components/Main/Recommends/MainItemTitle.js
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useContext, useEffect } from "react";
+import AuthContext from "../../../store/auth-context";
 
 const MainItemTitle = (props) => {
-  const [isLogin, setIsLogin] = useState(true);
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {}, []);
 
   return (
     <div>
-      {isLogin && (
-        <h2>
-          강아람<span>님을 위한 추천</span>
-        </h2>
+      {authCtx.isLoggedIn && (
+        <div>
+          <h2 style={{ display: "inline-block" }}>
+            {localStorage.getItem("nickname")}
+          </h2>
+          <span>님을 위한 추천 상품</span>
+        </div>
       )}
-      {!isLogin && <h2>인기 상품</h2>}
+      <div>{!authCtx.isLoggedIn && <h2>인기 상품</h2>}</div>
     </div>
   );
 };
