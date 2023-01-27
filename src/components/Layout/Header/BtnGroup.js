d2f3a7de5346765bf5412bf48d80562428547733
강아람 <001229aram@gmail.com>
2023-01-27 21:50:42 +0900

    design: 헤더 버튼 디자인 수정
    2023-01-27 디자인 수정을 위해 클래스 이름 변경,

--- src/components/Layout/Header/BtnGroup.js
+++ src/components/Layout/Header/BtnGroup.js
@@ -6,20 +6,24 @@ import classes from "./BtnGroup.module.css";
 
 const BtnGroup = (props) => {
   const authCtx = useContext(AuthContext);
+
   const handleLogoutClick = () => {
     authCtx.onLogout();
     window.location.href = "/";
   };
+
   return (
     <Fragment>
       {!authCtx.isLoggedIn && (
         <div className={classes["btn-group"]}>
-          <div className={classes["login-msg"]}>로그인을 해주세요.</div>
-          <div className={classes["bottom-btn"]}>
-            <Link to="/login" className={classes["left-btn"]}>
+          <div className={classes["btn-top"]}>
+            <p>로그인을 해주세요.</p>
+          </div>
+          <div className={classes["btn-bottom"]}>
+            <Link to="/login" className={classes["btn"]}>
               로그인
             </Link>
-            <Link to="/join" className={classes["right-btn"]}>
+            <Link to="/join" className={classes["btn"]}>
               회원가입
             </Link>
           </div>
@@ -28,25 +32,24 @@ const BtnGroup = (props) => {
       {authCtx.isLoggedIn && (
         <div className={classes["btn-group"]}>
           <Link to="/profile">
-            <div className={classes["avatar"]}>
-              <img
-                src={`${process.env.PUBLIC_URL}/assets/user.png`}
-                alt="유저 정보 수정 페이지 클릭"
-              />
-            </div>
-            <div className={classes["login-msg"]}>
-              <strong>{localStorage.getItem("nickname")}</strong>
-              <span>님 안녕하세요.</span>
+            <div className={classes["btn-top"]}>
+              <div className={classes["avatar"]}>
+                <img
+                  src={`${process.env.PUBLIC_URL}/assets/user.png`}
+                  alt="정보 수정 페이지 버튼"
+                />
+              </div>
+              <div>
+                <strong>{localStorage.getItem("nickname")}</strong>
+                <span>님 안녕하세요.</span>
+              </div>
             </div>
           </Link>
-          <div className={classes["bottom-btn"]}>
-            <Link to="/cart" className={classes["left-btn"]}>
+          <div className={classes["btn-bottom"]}>
+            <Link to="/cart" className={classes["btn"]}>
               장바구니
             </Link>
-            <button
-              className={classes["right-btn"]}
-              onClick={handleLogoutClick}
-            >
+            <button className={classes["btn"]} onClick={handleLogoutClick}>
               로그아웃
             </button>
           </div>
