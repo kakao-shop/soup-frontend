0e1498956d43a7e10d8f89708bbee9c4708d3de4
강아람 <001229aram@gmail.com>
2023-02-04 20:33:35 +0900

    Rename: auth로 api(token) 변경
    2023-02-04 api 폴더명, token api 파일명 변경

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,45 +1,45 @@
 import React, { useEffect, useState } from "react";
-import { removeCookie } from "../api/cookie";
+import { removeCookie } from "../apis/cookie";
 
 const AuthContext = React.createContext({
-  isLoggedIn: false,
-  onLogin: () => {},
-  onLogout: () => {},
+    isLoggedIn: false,
+    onLogin: () => {},
+    onLogout: () => {},
 });
 
 export const AuthContextProvider = (props) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    const storedUserInfo = localStorage.getItem("nickname");
+    useEffect(() => {
+        const storedUserInfo = localStorage.getItem("nickname");
 
-    if (storedUserInfo) {
-      setIsLoggedIn(true);
-    }
-  }, []);
+        if (storedUserInfo) {
+            setIsLoggedIn(true);
+        }
+    }, []);
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    };
 
-  const handleLogout = () => {
-    localStorage.clear();
-    removeCookie("accessToken");
-    removeCookie("refreshToken");
-    setIsLoggedIn(false);
-  };
+    const handleLogout = () => {
+        localStorage.clear();
+        removeCookie("accessToken");
+        removeCookie("refreshToken");
+        setIsLoggedIn(false);
+    };
 
-  return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-        onLogin: handleLogin,
-        onLogout: handleLogout,
-      }}
-    >
-      {props.children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider
+            value={{
+                isLoggedIn: isLoggedIn,
+                onLogin: handleLogin,
+                onLogout: handleLogout,
+            }}
+        >
+            {props.children}
+        </AuthContext.Provider>
+    );
 };
 
 export default AuthContext;
